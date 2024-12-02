0176d92f0046e4d80d76f23cfc868a2d96c57e13
Shashank Janagam Chandra <sjanagam@stevens.edu>
2024-12-02 13:31:06 -0500

    tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'rvsqb6',
+  projectId: "rvsqb6",
+
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
